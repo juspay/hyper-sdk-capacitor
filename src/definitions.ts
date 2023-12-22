7105a1b92cf64e799480f12abcdfd30526ff2b4a
subhash <subhash.chandra@juspay.in>
2023-12-22 12:49:02 +0530

    fix: fixed typescript error for addListener for plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -4,8 +4,9 @@
  * This source code is licensed under the AGPL 3.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import type { Plugin } from '@capacitor/core';
 
-export interface HyperServicesPlugin {
+export interface HyperServicesPlugin extends Plugin {
   createHyperServices(clientId?: string, service?: string): Promise<void>;
 
   preFetch(payload: any): Promise<void>;
